1854f3765ac35ba0ed210705d516003f26969ef5
Hans Balgobin <balgobinhans@hotmail.com>
2013-01-01 23:58:34 +0000

    New exception for synching pairs

--- src/main/java/com/bluesmoke/farm/exception/InvalidTimeStampException.java
+++ src/main/java/com/bluesmoke/farm/exception/InvalidTimeStampException.java
@@ -1,11 +1,10 @@
 package com.bluesmoke.farm.exception;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Oblene
- * Date: 01/01/13
- * Time: 17:37
- * To change this template use File | Settings | File Templates.
- */
-public class InvalidTimeStampException {
+public class InvalidTimeStampException extends Exception {
+
+    @Override
+    public void printStackTrace()
+    {
+        System.err.println("The timestamps do not coincide");
+    }
 }
